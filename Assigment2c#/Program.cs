8a0f9c37faeb8b6ee3e1946bd2f5631eb47ab50e
Admin <Admin@SALMAAMRO>
2025-07-07 23:30:01 +0300

    Q1

--- Assigment2c#/Program.cs
+++ Assigment2c#/Program.cs
@@ -5,18 +5,19 @@
         static void Main(string[] args)
         {
             #region Q1
-            // Q1:Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
-            Console.WriteLine("Pls Enter Number : ");
-            int Num = int.Parse(Console.ReadLine());
-            if (Num % 3 == 0 && Num % 4 == 0)
-            {
-                Console.WriteLine("Yes");
-            }
-            else
-            {
-                Console.WriteLine("No");
-            }
+            //// Q1:Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            //Console.WriteLine("Pls Enter Number : ");
+            //int Num = int.Parse(Console.ReadLine());
+            //if (Num % 3 == 0 && Num % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
             #endregion
+
         }
     }
 }
