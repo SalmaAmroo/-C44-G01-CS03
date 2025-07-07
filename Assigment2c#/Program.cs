5a3b651712eb7f68393a404a4dc44719d8676956
Admin <Admin@SALMAAMRO>
2025-07-08 02:34:52 +0300

    Q11

--- Assigment2c#/Program.cs
+++ Assigment2c#/Program.cs
@@ -182,6 +182,43 @@ namespace Assigment2c_
 
             #endregion
 
+
+            #region Q11
+            ////Q11 : 11- Write a program to input the month number and print the number of days in that month.
+            //Console.Write("Enter month number (1 to 12): ");
+            //int month = int.Parse(Console.ReadLine());
+
+            //int days;
+
+            //switch (month)
+            //{
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 7:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        days = 31;
+            //        break;
+            //    case 4:
+            //    case 6:
+            //    case 9:
+            //    case 11:
+            //        days = 30;
+            //        break;
+            //    case 2:
+            //        days = 28;
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid month number!");
+            //        return;
+            //}
+
+            //Console.WriteLine($"Number of days in month {month} = {days}");
+
+            #endregion
+
         }
     }
 }
